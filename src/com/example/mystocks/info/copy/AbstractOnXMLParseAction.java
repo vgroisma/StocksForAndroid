b6c40089fca5a4e61ed4bb2ebfcfd0527732c17d
vgroisma <v.groisman@gmail.com>
2014-08-05 12:26:56 +0300

    sync

--- src/com/example/mystocks/info/copy/AbstractOnXMLParseAction.java
+++
@@ -1,8 +0,0 @@
-package com.example.mystocks.info.copy;
-
-import java.util.HashMap;
-
-public abstract class AbstractOnXMLParseAction
-{
-	abstract void doAction(HashMap<EStockAttributes, String> xmlPullParserResults);
-}
